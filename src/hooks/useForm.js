dbefd4ad5c0da527c6a0b516cbc052ca244177fe
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-06-12 10:25:23 -0500

    initial set up

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+
+import React, {useState} from 'react' 
+
+import {useLocalStorage} from './useLocalStorage'
+
+export const useForm = (key, initialValue) => {
+    const [formValue, setFormValue] = useLocalStorage(key, initialValue);
+
+    return [formValue, setFormValue];
+}
