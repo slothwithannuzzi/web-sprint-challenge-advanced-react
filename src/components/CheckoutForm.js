3e0c3a3a714dc94e0d4a096e49a342886f560456
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-06-12 12:14:34 -0500

    wrote proper tests

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,16 +1,6 @@
 import React, { useState } from "react";
 import {useForm} from '../hooks/useForm'
 
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
-
-
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
@@ -28,6 +18,7 @@ const CheckoutForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setShowSuccessMessage(true);
     setFirstName('');
     setLastName('');
     setAddress('');
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const header = screen.getByText(/checkout form/i)
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const address = screen.getByLabelText(/address/i);
+    const city = screen.getByLabelText(/city/i);
+    const state = screen.getByLabelText(/state/i);
+    const zip = screen.getByLabelText(/zip/i);
+    const submitButton = screen.getByRole('button', {name: /checkout/i})
+
+
+    userEvent.type(firstName, 'John');
+    userEvent.type(lastName, 'Iannuzzi')
+    userEvent.type(address, '123 uzi lane');
+    userEvent.type(city, 'Uziville');
+    userEvent.type(state, 'uzopia');
+    userEvent.type(zip, '666666');
+    userEvent.click(submitButton);
+
+    const success = screen.getAllByTestId(/successMessage/i);
+
+    expect(success).toBeInTheDocument;
+});
